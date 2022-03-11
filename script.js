2c5be68623f04925cba367594ba6dec057c450a9
Kelvi03 <98356980+Kelvi03@users.noreply.github.com>
2022-03-11 22:47:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("Learn About Fish");
+
+console.log("THis will print to the console")
+function changeTEXT() {
+  document. getElementById("added").innerHTML="About FISH"
+}
+
+let signedIn= true;
+let age = 25
+
+let age1= 2.3
+let age2 = 2.343434
+
+function addTwoNumbers(a,b){
+  return a + b;
+
+}
+
+addTwoNumbers(1,2);
+
+console. log(addTwoNumbers(1,2))
